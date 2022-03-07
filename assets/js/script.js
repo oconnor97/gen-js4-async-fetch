af19a5d0d43714c8ca68460ef3135fbe4995c9f1
Victoria <70860669+victoriafreder@users.noreply.github.com>
2022-03-07 14:45:12 -0600

    clean up whitespace and comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,15 +15,13 @@ function getApi() {
       //looping over the fetch response and inserting the URL of your repos into a list
       console.log(data);
       for (let i = 0; i < data.length; i++) {
+        //Create a list element
         const li = document.createElement('li');
+        //Set the text of the list element to the JSON response's .html_url property
         li.innerHTML = data[i].html_url;
+        //Append the li element to the id associated with the ul element.
         repoList.appendChild(li);
       }
-      //Create a list element
-      
-      
-      //Set the text of the list element to the JSON response's .html_url property
-      //Append the li element to the id associated with the ul element.
     });
 }
 
