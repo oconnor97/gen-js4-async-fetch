e1022a4010135ed4e30af4a5449d017c4a63012e
Val <vdecyatnik@gmail.com>
2022-03-07 10:58:18 -0800

    fetch request activity

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ var fetchButton = document.getElementById("fetch-button");
 
 function getApi() {
   // Insert the API url to get a list of your repos
-  var requestUrl = "";
+  var requestUrl = " https://api.github.com/users/vdecyatnik/repos";
 
   fetch(requestUrl)
     .then(function (response) {
@@ -13,6 +13,15 @@ function getApi() {
     })
     .then(function (data) {
       //looping over the fetch response and inserting the URL of your repos into a list
+      console.log(data);
+      for (var i = 0; i < data.length; i++) {
+        console.log(data[i].html_url);
+        let liEl = document.createElement("li");
+        let htmlurl = data[i].html_url;
+        liEl.textContent = htmlurl;
+
+        repoList.append(liEl);
+      }
       //Create a list element
       //Set the text of the list element to the JSON response's .html_url property
       //Append the li element to the id associated with the ul element.
