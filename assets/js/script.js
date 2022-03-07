27457356e33790acf2fb4c0089057f5fb1bba961
Victoria <70860669+victoriafreder@users.noreply.github.com>
2022-03-07 14:43:57 -0600

    create fetch request

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ var fetchButton = document.getElementById("fetch-button");
 
 function getApi() {
   // Insert the API url to get a list of your repos
-  var requestUrl = "";
+  var requestUrl = "https://api.github.com/users/victoriafreder/repos";
 
   fetch(requestUrl)
     .then(function (response) {
@@ -13,7 +13,15 @@ function getApi() {
     })
     .then(function (data) {
       //looping over the fetch response and inserting the URL of your repos into a list
+      console.log(data);
+      for (let i = 0; i < data.length; i++) {
+        const li = document.createElement('li');
+        li.innerHTML = data[i].html_url;
+        repoList.appendChild(li);
+      }
       //Create a list element
+      
+      
       //Set the text of the list element to the JSON response's .html_url property
       //Append the li element to the id associated with the ul element.
     });
