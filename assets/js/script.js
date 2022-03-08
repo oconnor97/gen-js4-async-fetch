b1cca5848ddab3b3c9f70b0a4b7f25bd814ec752
Derrick James <95151579+flx1derrick@users.noreply.github.com>
2022-03-08 17:30:21 -0600

    console log urls working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,12 @@
-var repoList = document.querySelector("ul");
-var fetchButton = document.getElementById("fetch-button");
+let repoList = document.querySelector("ul");
+let fetchButton = document.getElementById("fetch-button");
+let li = document.createElement('li');
 
 //Create a getApi function that is called when the fetchButton is clicked
 
 function getApi() {
   // Insert the API url to get a list of your repos
-  var requestUrl = "";
+  var requestUrl = "https://api.github.com/users/flx1derrick/repos";
 
   fetch(requestUrl)
     .then(function (response) {
@@ -13,9 +14,17 @@ function getApi() {
     })
     .then(function (data) {
       //looping over the fetch response and inserting the URL of your repos into a list
+
+      for (const key of Object.keys(data)) {
+                console.log(`${data[key].html_url}`);
+                
+      }
       //Create a list element
+
       //Set the text of the list element to the JSON response's .html_url property
+
       //Append the li element to the id associated with the ul element.
+
     });
 }
 
