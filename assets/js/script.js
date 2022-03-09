a565ecd61ecaeb879f079a7c30c24548a36091f5
Derrick James <95151579+flx1derrick@users.noreply.github.com>
2022-03-08 18:59:55 -0600

    declare myRepos as requestURL

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,17 @@
 const repoList = document.querySelector("ul");
 const fetchButton = document.getElementById("fetch-button");
-
+const myRepos = "https://api.github.com/users/flx1derrick/repos";
 
 //Create a getApi function that is called when the fetchButton is clicked
 
 const getApi = (requestUrl) => {
-  requestUrl = "https://api.github.com/users/flx1derrick/repos";
+  requestUrl = myRepos;
   fetch(requestUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
       for (const key of Object.keys(data)) {
-               // console.log(`${data[key].html_url}`);
                let li = document.createElement('li')
                li.innerHTML +=  '<a href="' + data[key].html_url + '">' + data[key].html_url +'</a>'
                repoList.appendChild(li);
