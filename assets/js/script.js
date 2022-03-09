8ba7ba2b16fc0233487e11a76e638ae28154f5cf
Derrick James <95151579+flx1derrick@users.noreply.github.com>
2022-03-08 18:21:07 -0600

    add clickable link to repos and remove li bullets

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,22 @@
-let repoList = document.querySelector("ul");
-let fetchButton = document.getElementById("fetch-button");
-let li = document.createElement('li');
+const repoList = document.querySelector("ul");
+const fetchButton = document.getElementById("fetch-button");
+
 
 //Create a getApi function that is called when the fetchButton is clicked
 
 function getApi() {
-  // Insert the API url to get a list of your repos
   var requestUrl = "https://api.github.com/users/flx1derrick/repos";
-
   fetch(requestUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      //looping over the fetch response and inserting the URL of your repos into a list
-
       for (const key of Object.keys(data)) {
-                console.log(`${data[key].html_url}`);
-                
-      }
-      //Create a list element
-
-      //Set the text of the list element to the JSON response's .html_url property
-
-      //Append the li element to the id associated with the ul element.
-
-    });
+               // console.log(`${data[key].html_url}`);
+               let li = document.createElement('li')
+               li.innerHTML +=  '<a href="' + data[key].html_url + '">' + data[key].html_url +'</a>'
+               repoList.appendChild(li);
+      }});
 }
 
 fetchButton.addEventListener("click", getApi);
